0e38da40df84a5a83b8e636a9d42018d04f72df3
Niobos <niobos@dest-unreach.be>
2025-08-17 13:32:10 +0200

    Redid test cases for EnergyStates

--- tests/test_energy_states.py
+++ tests/test_energy_states.py
@@ -4,19 +4,21 @@ from pymitsubishi import EnergyStates
 
 
 @pytest.mark.parametrize(
-    "data_hex, operating",
+    "data_hex, this_unit, other_units",
     [  #  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-        ("fc6201301006000000000001004100004200000000d3", False),  # all units off
-        ("fc6201301006000000000001004100004200000000d3", False),  # this indoor unit off, other on
-        ("fc62013010060000000102b951e50000420000000023", True),  # only this indoor unit on, others off
-        ("fc6201301006000000010014004100004200000000bf", True),  # all indoor units on
-        ("fc620130100600000000073851ec0000420000000099", True),  # all indoor units on
-        ("fc62013010060000000106e451ed00004200000000ec", True),  # all indoor units on
+        ("fc6201301006000000000001004700004200000000cd", False, False),
+        ("fc620130100600000000001a52280000420000000081", False, False),
+        ("fc6201301006000000010004004700004200000000c9", True, False),
+        ("fc620130100600000000016652280000420000000034", False, True),
+        ("fc620130100600000001025552290000420000000042", True, True),
+        ("fc620130100600000001000e004700004200000000bf", True, True),
+        ("fc620130100600000000000a004700004200000000c4", False, True),
+        ("fc62013010060000000102095229000042000000008e", True, False),
     ],
 )
-def test_operating(data_hex, operating):
+def test_operating(data_hex, this_unit, other_units):
     state = EnergyStates.parse_energy_states(data_hex)
-    assert state.operating == operating
+    assert state.operating == this_unit
 
 
 @pytest.mark.parametrize(
